7daddbca925f2695d47b21277a707c647059d0b5
Joshua Balloch <joshuaballoch@gmail.com>
2013-07-10 14:05:55 +0000

    attemp to use index.html

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
+require('fs');
+
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  index_content = fs.readFileSync("index.html");
+  response.send(index_content);
 });
 
 var port = process.env.PORT || 5000;
